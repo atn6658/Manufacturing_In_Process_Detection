b14e913a8bc6666fa1eecf3932a87607dfc0056f
Minh Tram <minh.tram@mavs.uta.edu>
2024-10-12 22:00:51 -0500

    working model imports

--- Unity-Demo/Assets/Scripts/OBJImporter.cs
+++ Unity-Demo/Assets/Scripts/OBJImporter.cs
@@ -10,67 +10,44 @@ using UnityEngine.Networking;
 public class OBJImporter : MonoBehaviour
 {
     public TMPro.TMP_InputField FilePathInput;
+    public TMPro.TMP_Dropdown PartsDropdown;
     public List<GameObject> ImportedObjects;
     public GameObject SelectedObject;
+    public Material HighlighMaterial;
+    public Material DefaultMaterial;
 
     int ObjectCounter = 1;
-    String fileContent = string.Empty;
 
     void Start()
     {
         ImportedObjects = new List<GameObject>();
+        PartsDropdown.ClearOptions();
     }
 
     void Update()
     {
     }
 
-    public void LoadModel()
-    {
-        string filePath = FilePathInput.text;
-        // trim leading and ending quotes and spaces
-        filePath = filePath.Trim('"').Trim();
-        // attempt to load the model
-        // silently aborting if error at any point
-        try
-        {
-            GameObject loadedObject = null;
-            if (filePath.StartsWith("http") || filePath.StartsWith("https"))
-            {
-                StartCoroutine(GetStreamFromURL(filePath));
-                loadedObject = new OBJLoader().Load(new MemoryStream(Encoding.UTF8.GetBytes(fileContent)));
-            }
-            else
-            {
-                throw new Exception("Only HTTP/HTTPS URLs are supported for now.");
-            }
-            // {
-            //     string completePath = Application.streamingAssetsPath + "/" + filePath;
-            //     Debug.Log("Loading file from: " + completePath);
-            //     StartCoroutine(GetStreamFromURL(completePath));
-            //     loadedObject = new OBJLoader().Load(new MemoryStream(Encoding.UTF8.GetBytes(fileContent)));
-            // }
-            // set the object's name
-            loadedObject.name = "ImportedObject" + ObjectCounter++;
-            // scale down object to mm
-            loadedObject.transform.localScale *= 0.001f;
-            // set the object's parent
-            loadedObject.transform.SetParent(transform);
-            // add the object to the list
-            ImportedObjects.Add(loadedObject);
-        }
-        catch (System.Exception e)
-        {
-            Debug.LogError("Error loading model: " + e.Message);
-            return;
-        }
-        // clear the input field
-        FilePathInput.text = string.Empty;
-    }
+//     IEnumerator LoadModel()
+//     {
+//         string completePath = Application.streamingAssetsPath + "/" + FilePathInput.text;
+//         Debug.Log("Loading file from: " + completePath);
+// #pragma warning disable CS0618 // Type or member is obsolete
+//         using (var www = new WWW(completePath))
+//         {
+//             yield return www;
+//             var stream = new MemoryStream(Encoding.UTF8.GetBytes(www.text));
+//             var loadedObject = new OBJLoader().Load(stream);
+//         }
+// #pragma warning restore CS0618 // Type or member is obsolete
+//     }
 
-    IEnumerator GetStreamFromURL(string url)
+    public void InvokeLoadModel() => StartCoroutine(LoadModel((Application.streamingAssetsPath + "/" + FilePathInput.text).Trim('"').Trim()));
+
+    IEnumerator LoadModel(string URL)
     {
-        using (UnityWebRequest www = UnityWebRequest.Get(url))
+        Debug.Log("Loading file from: " + URL);
+        using (UnityWebRequest www = UnityWebRequest.Get(URL))
         {
             yield return www.SendWebRequest();
             switch (www.result)
@@ -78,12 +55,61 @@ public class OBJImporter : MonoBehaviour
                 case UnityWebRequest.Result.ConnectionError:
                 case UnityWebRequest.Result.DataProcessingError:
                 case UnityWebRequest.Result.ProtocolError:
-                    Debug.LogError("Error loading model: " + www.error);
+                    Debug.LogError("Error: " + www.error);
                     break;
                 case UnityWebRequest.Result.Success:
-                    fileContent = www.downloadHandler.text;
+                    string content = www.downloadHandler.text;
+                    var textStream = new MemoryStream(Encoding.UTF8.GetBytes(content));
+                    var loadedObject = new OBJLoader().Load(textStream);
+                    // set scale to mm
+                    loadedObject.transform.localScale *= 0.001f;
+                    // rename object
+                    loadedObject.name = Path.GetFileNameWithoutExtension(FilePathInput.text) + "_" + ObjectCounter++;
+                    // set object parent
+                    loadedObject.transform.SetParent(transform);
+                    // apply default material
+                    foreach (var renderer in loadedObject.GetComponentsInChildren<MeshRenderer>())
+                        renderer.material = DefaultMaterial;
+                    // add to list
+                    ImportedObjects.Add(loadedObject);
                     break;
             }
         }
+
+        // clear the input field
+        // FilePathInput.text = string.Empty;
+
+        // update dropdown
+        UpdateDropdown();
+    }
+
+    void UpdateDropdown()
+    {
+        PartsDropdown.ClearOptions();
+        // skip if no objects are imported
+        if (ImportedObjects.Count == 0) return;
+        // get object names
+        List<string> objectNames = new List<string>();
+        foreach (var obj in ImportedObjects)
+        {
+            objectNames.Add(obj.name);
+        }
+        // update dropdown
+        PartsDropdown.AddOptions(objectNames);
+        // update option if it's the first object
+        if (ImportedObjects.Count == 1)
+            SelectedObject = ImportedObjects[0];
+    }
+
+    public void UpdateDropdownTarget()
+    {
+        // reset color of previous object
+        if (SelectedObject != null)
+            foreach (var renderer in SelectedObject.GetComponentsInChildren<MeshRenderer>())
+                renderer.material = DefaultMaterial;
+        SelectedObject = ImportedObjects[PartsDropdown.value];
+        // highlight selected object
+        foreach (var renderer in SelectedObject.GetComponentsInChildren<MeshRenderer>())
+            renderer.material = HighlighMaterial;
     }
 }
